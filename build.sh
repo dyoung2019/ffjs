73fd721f2c9eb2a84ed09becad8d7dfe36b74a4b
David Young <tgsstdio@gmail.com>
2023-01-05 15:59:26 +1100

    initial ffmpeg changes

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bash
-cmake -DCMAKE_TOOLCHAIN_FILE=./emscripten/cmake/Modules/Platform/Emscripten.cmake .
+cmake -DCMAKE_TOOLCHAIN_FILE=./emscripten/cmake/Modules/Platform/Emscripten.cmake -DEMSCRIPTEN_ROOT_PATH=/usr/local/opt/emscripten/bin/ .
